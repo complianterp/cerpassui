c91bb9516501ee121ce0eaa025190f20ad2fdc6a
P846802 <neil@princeglobal.net>
2018-04-11 04:31:11 +0000

    Initial commit

---
+++ ui/webapp/controller/main_menu.controller.js
@@ -0,0 +1,175 @@
+sap.ui.define([
+	'jquery.sap.global',
+	'sap/ui/core/Fragment',
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/model/json/JSONModel',
+	'sap/m/Popover',
+	'sap/m/Button'
+], function (jQuery, Fragment, Controller, JSONModel, Popover, Button) {
+	"use strict";
+
+	var CController = Controller.extend("cerpassui.ui.controller.main_menu", {
+		model : new sap.ui.model.json.JSONModel(),
+		data : {
+			navigation: [{
+				title: 'Dashboard',
+				icon: 'sap-icon://employee',
+				expanded: false,
+				key: 'root0'
+			},{
+				title: 'Master Data',
+				icon: 'sap-icon://employee',
+				expanded: false,
+				key: 'root1',
+				items: [{
+					title: 'Modules',
+					key: 'page1'
+				}, {
+					title: 'Activities',
+					key: 'page2'
+				}]
+			}, {
+				title: 'Transaction',
+				icon: 'sap-icon://building',
+				key: 'root2',
+				expanded: false,
+				items: [{
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}]
+			}, {
+				title: 'Reporting',
+				icon: 'sap-icon://card',
+				expanded: false,
+				items: [{
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}, {
+					title: 'Child Item'
+				}]
+			}, {
+				title: 'Source Data',
+				icon: 'sap-icon://action',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}, {
+				title: 'Settings',
+				icon: 'sap-icon://action-settings',
+				expanded: false,
+				items: [{
+					title: 'Child Item 1'
+				}, {
+					title: 'Child Item 2'
+				}, {
+					title: 'Child Item 3'
+				}]
+			}
+			],
+			fixedNavigation: [{
+				title: 'Fixed Item 1',
+				icon: 'sap-icon://employee'
+			}, {
+				title: 'Fixed Item 2',
+				icon: 'sap-icon://building'
+			}, {
+				title: 'Fixed Item 3',
+				icon: 'sap-icon://card'
+			}],
+			headerItems: [
+			{
+				text: "File"
+			}, {
+				text: "Edit"
+			}, {
+				text: "View"
+			}, {
+				text: "Settings"
+			}, {
+				text: "Help"
+			}]
+		},
+		onInit : function() {
+			this.model.setData(this.data);
+			this.getView().setModel(this.model);
+
+			this._setToggleButtonTooltip(!sap.ui.Device.system.desktop);
+		},
+
+		onItemSelect : function(oEvent) {
+			var item = oEvent.getParameter('item');
+			var viewId = this.getView().getId();
+			sap.ui.getCore().byId(viewId + "--pageContainer").to(viewId + "--" + item.getKey());
+		},
+
+		handleUserNamePress: function (event) {
+			var popover = new Popover({
+				showHeader: false,
+				placement: sap.m.PlacementType.Bottom,
+				content:[
+					new Button({
+						text: 'Feedback',
+						type: sap.m.ButtonType.Transparent
+					}),
+					new Button({
+						text: 'Help',
+						type: sap.m.ButtonType.Transparent
+					}),
+					new Button({
+						text: 'Logout',
+						type: sap.m.ButtonType.Transparent
+					})
+				]
+			}).addStyleClass('sapMOTAPopover sapTntToolHeaderPopover');
+
+			popover.openBy(event.getSource());
+		},
+
+		onSideNavButtonPress : function() {
+			var viewId = this.getView().getId();
+			var toolPage = sap.ui.getCore().byId(viewId + "--toolPage");
+			var sideExpanded = toolPage.getSideExpanded();
+
+			this._setToggleButtonTooltip(sideExpanded);
+
+			toolPage.setSideExpanded(!toolPage.getSideExpanded());
+		},
+
+		_setToggleButtonTooltip : function(bLarge) {
+			var toggleButton = this.getView().byId('sideNavigationToggleButton');
+			if (bLarge) {
+				toggleButton.setTooltip('Large Size Navigation');
+			} else {
+				toggleButton.setTooltip('Small Size Navigation');
+			}
+		}
+
+	});
+
+
+	return CController;
+
+});
